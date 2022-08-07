6503e0b4dbbd15517f527bb72efb1e8a6c4347b0
Luis <luis@paladin.insure>
2022-08-07 16:11:59 -0500

    Added git and github section. Also removed dummy data from app.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
@@ -13,14 +10,7 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 100 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
